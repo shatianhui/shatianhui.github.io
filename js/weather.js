16382c003f604a1e806fdd5ae3572fc0add6a8b3
Sha Tianhui <3272027591@qq.com>
2022-09-23 20:05:36 +0800

    Site updated: 2022-09-23 20:05:35

---
+++ js/weather.js
@@ -0,0 +1,24 @@
+WIDGET = {
+    "CONFIG": {
+        "modules": "01234",
+        "background": "5",
+        "tmpColor": "4A86E8",
+        "tmpSize": "16",
+        "cityColor": "FF9900",
+        "citySize": "16",
+        "aqiColor": "4A86E8",
+        "aqiSize": "16",
+        "weatherIconSize": "24",
+        "alertIconSize": "18",
+        "padding": "10px 10px 10px 10px",
+        "shadow": "0",
+        "language": "auto",
+        "fixed": "true",
+        "vertical": "center",
+        "horizontal": "center",
+        "left": "179",
+        "top": "10",
+        "key": "a057c29725924ddf9ba9e0890cdaaf0c"
+    }
+}
+
